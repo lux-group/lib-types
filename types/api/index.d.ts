2fb7d5cab66f12c60f6dd1c4c2735d1bf49e256e
Rufus Post <rufuspost@gmail.com>
2019-10-10 15:01:38 +1300

    adds exports

--- types/api/index.d.ts
+++ types/api/index.d.ts
@@ -1,2 +1,7 @@
+import { Auth } from "./auth";
+import { Discovery } from "./discovery";
+import { Flights } from "./flights";
+import { Loyalty } from "./loyalty";
 import { PublicOffer } from "./public-offer";
-export { PublicOffer };
+import { Wishlist } from "./wishlist";
+export { Auth, Discovery, Flights, Loyalty, PublicOffer, Wishlist };
