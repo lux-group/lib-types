7a99acdfda64617f320d266e14add8f2047a6929
Alexander Sokol <reb2020@gmail.com>
2021-03-12 13:03:18 +0200

    update types

---
+++ types/api/bedbank.d.ts
@@ -0,0 +1,215 @@
+export namespace Bedbank {
+  interface StrObject {
+    [field: string]: string;
+  }
+  
+  interface ReservationCancellationPolicy {
+    currency: string;
+    start: string;
+    end: string;
+    percent: number | null;
+    amount: number | null;
+    nights: number | null;
+  }
+
+  interface Configuration {
+    type: string;
+    size: string;
+    quantity: number;
+  }
+
+  interface ReservationBedGroup {
+    id: string;
+    description: string;
+    configuration: Configuration[];
+  }
+
+  interface OccupancyFee {
+    type: string;
+    amount: number;
+    scope: string;
+    frequency: string;
+  }
+
+  interface ReservationRoom {
+    id: string;
+    confirmationId: string;
+    name: string;
+    status: string;
+    roomIndex: number;
+    refundable: boolean;
+    price: number;
+    exclusivePrice: number;
+    taxesAndFees: number;
+    salesTax: number;
+    costPriceInclusive: number;
+    marketingFee: number;
+    fees: OccupancyFee[];
+    guestFirstName: string;
+    guestLastName: string;
+    guestSpecialRequests: string;
+    numberOfAdults: number;
+    numberOfChildren: number;
+    childrenAges: number[];
+    facilities: string[];
+    bedGroup: ReservationBedGroup;
+    cancellationPolicies: Array<ReservationCancellationPolicy>;
+  }
+  
+  interface Reservation {
+    id: string;
+    state: string;
+    transactionKey: string;
+    externalId: string;
+    propertyId: string;
+    roomId: string;
+    roomRateId: string;
+    bookingEmail: string;
+    bookingPhone: string;
+    checkIn: string;
+    checkOut: string;
+    nights: number;
+    totalPrice: number;
+    exclusivePrice: number;
+    taxesAndFees: number;
+    propertyFees: number;
+    costPriceInclusive: number;
+    marketingFee: number;
+    rooms: Array<ReservationRoom>;
+  }
+
+  interface RateCancellationPolicy {
+    currency: string;
+    start: string;
+    end: string;
+    percent?: number;
+    amount?: number;
+    nights?: number;
+  }
+
+  interface PropertyLocation {
+    longitude: number;
+    latitude: number;
+  }
+
+  interface CapacityResponse {
+    id: string;
+    adults: number;
+    children: number;
+    infants: number;
+  }
+  
+  interface AgeCategoryResponse {
+    id: string;
+    name: string;
+    minimumAge: number;
+  }
+  
+  interface BedGroupResponse {
+    id: string;
+    description: string;
+  }
+  
+  interface FacilityGroups {
+    name: string;
+    values: Array<{
+      name: string;
+    }>;
+  }
+  
+  interface RateOccupancyPricingResponse {
+    exclusive: number;
+    inclusive: number;
+    taxesAndFees: number;
+    occupancy: string;
+    fees: OccupancyFee[];
+    salesTax: number;
+  }
+
+  interface RoomRateTotalResponse {
+    exclusive: number;
+    inclusive: number;
+    taxesAndFees: number;
+    propertyFees?: number;
+  }
+
+  interface RoomRateResponse {
+    id: string;
+    refundable: boolean;
+    regionCode: string;
+    currencyCode: string;
+    cancellationPolicies: Array<RateCancellationPolicy>;
+    occupancyPricing: Array<RateOccupancyPricingResponse>;
+    nights?: number;
+    facilities: string[];
+    totals: RoomRateTotalResponse;
+    bedGroups: BedGroupResponse[];
+    price: number;
+    value: number;
+    discount: number;
+  }
+  
+  interface ImageResponse {
+    id: string | null;
+  }
+  
+  interface RoomTypeResponse {
+    id: string;
+    name: string;
+    description: string;
+    images: ImageResponse[];
+    roomRates: RoomRateResponse[];
+    capacities: {
+      combinations: CapacityResponse[];
+      ageCategories: AgeCategoryResponse[];
+    };
+    facilityGroups: Array<FacilityGroups>;
+  }
+  
+  interface PropertyAddressResponse {
+    line1?: string;
+    line2?: string;
+    city?: string;
+    stateProvinceCode?: string;
+    stateProvinceName?: string;
+    postalCode?: string;
+    countryCode?: string;
+    countryName?: string;
+    obfuscationRequired: boolean;
+  }
+  
+  interface PropertyFinePrint {
+    checkIn: {
+      beginTime?: string;
+      endTime?: string;
+      instructions?: string;
+      specialInstructions?: string;
+    };
+    checkOut: {
+      time?: string;
+    };
+    fees: {
+      mandatory?: string;
+      optional?: string;
+    };
+    policies?: StrObject;
+    finePrintPets?: Array<string | undefined>;
+  }
+
+  interface PropertyResponse {
+    id: string;
+    name: string;
+    slug: string;
+    roomTypes: RoomTypeResponse[];
+    images: ImageResponse[];
+    popularFacilities: Array<string>;
+    facilityGroups: Array<FacilityGroups>;
+    address: PropertyAddressResponse;
+    attractions?: string;
+    location: PropertyLocation;
+    phone: string;
+    propertyFinePrint: PropertyFinePrint;
+    description: string;
+    metaDescription?: string;
+  }
+}
---
+++ types/api/public-offer-v2.d.ts
@@ -0,0 +1,144 @@
+export namespace PublicOfferV2 {
+  interface StrObject {
+    [field: string]: string;
+  }
+
+  interface FacilityGroups {
+    name: string;
+    values: Array<{
+      name: string;
+    }>;
+  }
+
+  interface Image {
+    id: string;
+  }
+
+  interface Configuration {
+    type: string;
+    size: string;
+    quantity: number;
+  }
+
+  interface BedGroup {
+    id: string;
+    description: string;
+    configuration: Configuration[];
+  }
+
+  interface RateOccupancyPricing {
+    exclusive: number;
+    inclusive: number;
+    taxesAndFees: number;
+    occupancy: string;
+    fees: OccupancyFee[];
+    salesTax: number;
+  }
+
+  interface RateCancellationPolicy {
+    currency: string;
+    start: string;
+    end: string;
+    percent?: number;
+    amount?: number;
+    nights?: number;
+  }
+
+  interface RoomRateTotal {
+    exclusive: number;
+    inclusive: number;
+    taxesAndFees: number;
+    propertyFees?: number;
+  }
+
+  interface RoomRate {
+    id: string;
+    refundable: boolean;
+    regionCode: string;
+    currencyCode: string;
+    cancellationPolicies: Array<RateCancellationPolicy>;
+    occupancyPricing: Array<RateOccupancyPricing>;
+    totals: RoomRateTotal;
+    nights: number;
+    facilities: string[];
+    bedGroups: Array<BedGroup>;
+    value: number;
+    price: number;
+    discount: number;
+  }
+
+  interface Capacity {
+    combinations: Array<{
+      adults: number;
+      children: number;
+      infants: number;
+    }>;
+    ageCategories: Array<{
+      name: string;
+      minimumAge: number;
+    }>;
+  }
+
+  interface Package {
+    fkRoomTypeId: string;
+    rates: Array<RoomRate>;
+    name: string;
+    description: string;
+    images: Array<Image>;
+    facilityGroups: Array<FacilityGroups>;
+    capacities: Capacity;
+  }
+
+  interface PropertyAddressResponse {
+    line1?: string;
+    line2?: string;
+    city?: string;
+    stateProvinceCode?: string;
+    stateProvinceName?: string;
+    postalCode?: string;
+    countryCode?: string;
+    countryName?: string;
+    obfuscationRequired: boolean;
+  }
+
+  interface PropertyFinePrint {
+    checkIn: {
+      beginTime?: string;
+      endTime?: string;
+      instructions?: string;
+      specialInstructions?: string;
+    };
+    checkOut: {
+      time?: string;
+    };
+    fees: {
+      mandatory?: string;
+      optional?: string;
+    };
+    policies?: StrObject;
+    pets?: Array<string | undefined>;
+  }
+
+  interface Offer {
+    id: string;
+    type: "bedbank_hotel";
+    name: string;
+    slug: string;
+    description: string;
+    metaDescription: string;
+    packages: Array<Package>;
+    images: Array<Image>;
+    popularFacilities: Array<string>;
+    facilityGroups: Array<FacilityGroups>;
+    property: {
+      id: string;
+      address: PropertyAddressResponse;
+      location: {
+        longitude: number;
+        latitude: number;
+      };
+    };
+    attractions?: string;
+    propertyFinePrint: PropertyFinePrint;
+  }
+}
