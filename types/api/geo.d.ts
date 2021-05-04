513517d2c9ca21a9fad11bc374bc34cc0b8f4f25
Joshua Cullen <joshua.cullen@luxgroup.com>
2021-05-04 15:05:12 +1000

    Fix up geo types

--- types/api/geo.d.ts
+++ types/api/geo.d.ts
@@ -1,16 +1,16 @@
 export namespace Geo {
   interface Data {
-    continent_code: string;
+    continent_code: string | null;
     country: string;
     country_code: string;
     administrative_area_level_1: string;
-    administrative_area_level_2: string;
-    administrative_area_level_3: string;
-    administrative_area_level_4: string;
-    administrative_area_level_5: string;
-    locality: string;
-    route: string;
-    street_number: string;
+    administrative_area_level_2: string | null;
+    administrative_area_level_3: string | null;
+    administrative_area_level_4: string | null;
+    administrative_area_level_5: string | null;
+    locality: string | null;
+    route: string | null;
+    street_number: string | null;
     place_id: string;
   }
 }
