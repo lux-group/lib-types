0d2fcb5ae2fbe3b7462bee67a83e120ef10eb3a1
J-Leg <jamesz96.au@gmail.com>
2021-09-10 11:18:03 +1000

    revert lint move contents to .d.ts file

--- types/api/reporting.d.ts
+++ types/api/reporting.d.ts
@@ -1,1 +1,36 @@
-export declare namespace Reporting {}
+export namespace Reporting {
+  interface OrderStatsResponse {
+    message: string;
+    status: number;
+    result: OrderResult;
+  }
+
+  interface OrderResult {
+    [region: string]: OrderStats;
+  }
+
+  interface OrderStats {
+    period: {
+      from: string;
+      to: string;
+    };
+    min: number;
+    count: number;
+    is_popular: boolean;
+  }
+
+  interface OfferStatsResponse {
+    message: string;
+    status: number;
+    result: OfferResult;
+  }
+
+  type OfferResult = Array<OfferStats>;
+
+  interface OfferStats {
+    total_purchase: string;
+    offer_id: string;
+    page_views: string | null;
+    region: string;
+  }
+}
