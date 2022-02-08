5b6fba2a5af9157c09488f55fe61a8df6446fb86
jamesz96 <jamesz96.au@gmail.com>
2022-02-08 13:00:36 +1100

    lint fix

--- types/api/experiences.d.ts
+++ types/api/experiences.d.ts
@@ -9,7 +9,7 @@ export namespace Experiences {
 
   type FormData = MusementFormData;
 
-  type MusementParticipantItemSchema = Record<string, unknown>
+  type MusementParticipantItemSchema = Record<string, unknown>;
   type MusementFormData = {
     customerInfoSchema: Record<string, unknown>;
     participantInfoSchemaMap?: Record<string, MusementParticipantItemSchema>;
