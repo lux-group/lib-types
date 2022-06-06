b67f5e9f527cee89946070a52481541923590a65
greg-cumming-le <greg.cumming@luxuryescapes.com>
2022-06-07 08:58:29 +1200

    Add new referral types

---
+++ types/api/referral.d.ts
@@ -0,0 +1,59 @@
+
+// Referral types (svc-promo)
+
+export namespace Referral { 
+  /**
+   * Aligns with the 'referral_log_status' db enum
+   */
+  type LogStatus = 'pending_cooldown'|'pending_selection'|'redeeming'|'redeemed'
+
+  /**
+   * Used to match Referral Log Types in the db to type of Earn Option
+   */
+  type EarnTypeCode = 'credit'|'qff'|'unknown'
+
+  interface EarnOption {
+    earn_option_id:string
+    display_name:string
+    earn_type_code:EarnTypeCode
+    long_description?:string
+    summary?:string
+  }
+
+  interface GetReferralEarnOptionsResponse {
+    status: 200
+    success: boolean
+    region: string
+    brand: string
+    referral_log_id:string,
+    user_id: string
+    result: {
+      earn_options: EarnOption[]
+    }
+  }
+
+  interface GetReferralLogsResponse {
+    status: 200
+    success:boolean
+    message?:string
+    result: {
+      referral_logs: ReferralLog[]
+      totals:{
+        pending:number
+        available:number
+        redeemed:number
+        total_amount_redeemed:number
+      }
+    }
+  }
+
+  interface ReferralLog {
+    amount?:string
+    referral_log_id:string
+    earn_details:string
+    earn_type?:EarnTypeCode
+    log_status:LogStatus
+    earn_amount?:string
+    first_name?: string
+  }
+}
