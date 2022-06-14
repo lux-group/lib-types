27969f45903cdf42d166dd6c2ce2457080cad52b
Henry Shen <83993302+henryshen-le@users.noreply.github.com>
2022-06-14 11:21:46 +1000

    add tour search types

--- types/api/search/tour.d.ts
+++ types/api/search/tour.d.ts
@@ -1,4 +1,5 @@
 import { Common } from "../common";
+import { TYPEAHEAD_TYPE } from "./typeahead";
 
 export namespace Tour {
   type SearchResult = Array<string>;
@@ -12,3 +13,15 @@ export namespace Tour {
 
   export type GetOfferIds200Response = Common.OkResponse<SearchResult>;
 }
+
+const POSSIBLE_TOUR_SEARCH_PLACE_TYPES = [
+  TYPEAHEAD_TYPE.CITY,
+  TYPEAHEAD_TYPE.MULTI_CITY_VICINITY,
+  TYPEAHEAD_TYPE.COLLOQUIAL_AREA,
+  TYPEAHEAD_TYPE.PROVINCE_STATE,
+  TYPEAHEAD_TYPE.COUNTRY,
+  TYPEAHEAD_TYPE.HIGH_LEVEL_REGION,
+  TYPEAHEAD_TYPE.CONTINENT,
+] as const;
+
+type TOUR_SEARCH_PLACE_TYPES = typeof POSSIBLE_TOUR_SEARCH_PLACE_TYPES[number];
