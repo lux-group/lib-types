09bcf92d50054388377b4a23504ba54061bc64cd
Henry Shen <83993302+henryshen-le@users.noreply.github.com>
2022-06-14 11:35:04 +1000

    renamed to ts

--- types/api/search/tour.d.ts
+++ types/api/search/tour.d.ts
@@ -1,5 +1,5 @@
 import { Common } from "../common";
-import { TYPEAHEAD_TYPE } from "./typeahead";
+import { POSSIBLE_TOUR_TYPEAHEAD_PLACE_TYPES } from "./typeahead";
 
 export namespace Tour {
   type SearchResult = Array<string>;
@@ -13,15 +13,5 @@ export namespace Tour {
 
   type GetOfferIds200Response = Common.OkResponse<SearchResult>;
 
-  const POSSIBLE_TYPEAHEAD_PLACE_TYPES = [
-    TYPEAHEAD_TYPE.CITY,
-    TYPEAHEAD_TYPE.MULTI_CITY_VICINITY,
-    TYPEAHEAD_TYPE.COLLOQUIAL_AREA,
-    TYPEAHEAD_TYPE.PROVINCE_STATE,
-    TYPEAHEAD_TYPE.COUNTRY,
-    TYPEAHEAD_TYPE.HIGH_LEVEL_REGION,
-    TYPEAHEAD_TYPE.CONTINENT,
-  ] as const;
-
-  type TYPEAHEAD_PLACE_TYPES = typeof POSSIBLE_TYPEAHEAD_PLACE_TYPES[number];
+  type TYPEAHEAD_PLACE_TYPES = typeof POSSIBLE_TOUR_TYPEAHEAD_PLACE_TYPES[number];
 }
