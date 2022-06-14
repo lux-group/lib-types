12db5ce12280eb09d042b2a90417a2499c8aea79
Henry Shen <83993302+henryshen-le@users.noreply.github.com>
2022-06-14 12:20:07 +1000

    add in all enums

--- types/api/search/tour.d.ts
+++ types/api/search/tour.d.ts
@@ -1,7 +1,7 @@
 import { Common } from "../common";
 import { Typeahead } from "./typeahead";
 
-declare namespace Tour {
+export namespace Tour {
   type SearchResult = Array<string>;
 
   interface ListOffersParams {
@@ -15,5 +15,11 @@ declare namespace Tour {
 
   enum PossibleTypeaheadPlaceTypes {
     CITY = Typeahead.TYPES.CITY,
+    PROVINCE_STATE = Typeahead.TYPES.PROVINCE_STATE,
+    HIGH_LEVEL_REGION = Typeahead.TYPES.HIGH_LEVEL_REGION,
+    COUNTRY = Typeahead.TYPES.COUNTRY,
+    MULTI_CITY_VICINITY = Typeahead.TYPES.MULTI_CITY_VICINITY,
+    CONTINENT = Typeahead.TYPES.CONTINENT,
+    COLLOQUIAL_AREA = Typeahead.TYPES.COLLOQUIAL_AREA,
   }
 }
